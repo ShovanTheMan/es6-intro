b9ce1e4f4205d7598a9b2509442ba0a3d515957b
ShovanTheMan <shovan2192@gmail.com>
2023-12-15 15:28:04 +0600

    default added

---
+++ js/default.js
@@ -0,0 +1,15 @@
+function add(first, second){
+    const total = first + second;
+    return total;
+}
+
+const result = add(10, 20);
+console.log(result);
+
+function fullName (first , last = 'chowdhury'){
+    const name = first + ' ' + last;
+    return name;
+}
+
+const name = fullName("Aslam");
+console.log(name);
