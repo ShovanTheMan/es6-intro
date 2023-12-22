03297413a029116892f29b451adfa689663650e2
ShovanTheMan <shovan2192@gmail.com>
2023-12-22 16:15:49 +0600

    more added

---
+++ js/arrow.js
@@ -0,0 +1,16 @@
+function add(first, second){
+    const total = first + second;
+    return total;
+}
+
+const add1 = function add1(first, second){
+    const total = first + second;
+    return total;
+}
+//arrow function
+const add6 = (first, second) => first + second;
+
+const result = add6(10, 20);
+console.log(result);
+
+//differences between function declaration and arrow funtion and function expression
